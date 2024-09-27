8d3b3ba2c1830bfc12afdd7c313cbdd19d1c3561
Junior Lopes <juniorlopes@Juniors-MacBook-Pro.local>
2024-09-26 22:00:17 -0300

    first commit - init project

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    host: "localhost",
+    dialect: 'mysql',
+    username: 'root',
+    password: 'admin123.',
+    port: 3306,
+    database: 'todos_scheme',
+    define: {
+        timestamp: true,
+        underscored: true,
+    },
+}
---
+++ src/database/migrations/20240927004623-create-users.js
@@ -0,0 +1,43 @@
+'use strict';
+
+//Criate migration: npx sequelize-cli migration:create --name=create-users
+//Run migration: npx sequelize-cli db:migrate 
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      // name: {
+      //   type: Sequelize.STRING,
+      //   allowNull: false,
+      // },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
